86e54bcc91997411edd9de5b76a7dd34746086d6
Goodlike <henrisssss@hotmail.com>
2016-07-01 14:15:23 +0300

    Added a few extra default values

--- src/main/java/eu/goodlike/twitch/TwitchDefaults.java
+++ src/main/java/eu/goodlike/twitch/TwitchDefaults.java
@@ -1,5 +1,7 @@
 package eu.goodlike.twitch;
 
+import static eu.goodlike.twitch.m3u8.M3U8Defaults.M3U8_MEDIA_PREFIX;
+
 public final class TwitchDefaults {
 
     public static final String VERSION_3_HEADER_NAME = "Accept";
@@ -9,6 +11,12 @@ public final class TwitchDefaults {
 
     public static final String OAUTH_HEADER_NAME = "Authorization";
     public static final String OAUTH_HEADER_VALUE_PREFIX = "OAuth ";
+
+    public static final String START_OFFSET_PARAM = "start_offset";
+    public static final String END_OFFSET_PARAM = "end_offset";
+    public static final String TWITCH_M3U8_MEDIA_PART_FORMAT =
+            M3U8_MEDIA_PREFIX + "{},{}" + System.lineSeparator() +
+            "{}{}?" + START_OFFSET_PARAM + "={}&" + END_OFFSET_PARAM + "={}";
 
     // PRIVATE
 
