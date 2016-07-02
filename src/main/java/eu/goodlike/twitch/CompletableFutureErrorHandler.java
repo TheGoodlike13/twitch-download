4469b3b6a4ae6d2a9a024caef70396b9fdae8302
Goodlike <henrisssss@hotmail.com>
2016-07-02 19:59:02 +0300

    Added CompletableFutureErrorHandler
    This handler is pretty basic, but it avoid too many error messages due to chained whenComplete() calls

---
+++ src/main/java/eu/goodlike/twitch/CompletableFutureErrorHandler.java
@@ -0,0 +1,45 @@
+package eu.goodlike.twitch;
+
+import eu.goodlike.functional.Futures;
+import eu.goodlike.io.log.CustomizedLogger;
+
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.Set;
+import java.util.function.BiConsumer;
+
+/**
+ * Handler for CompletableFuture errors, avoids unnecessary error messages for errors that have already occurred
+ */
+public final class CompletableFutureErrorHandler {
+
+    /**
+     * @return BiConsumer to be used in a whenComplete() of a CompletableFuture; this consumer ensures that error
+     * message for a particular error will be executed only once, the first time it is referenced
+     */
+    public <Result> BiConsumer<Result, Throwable> logOnError(String errorMessage) {
+        return Futures.completionHandler(
+                any -> {},
+                throwable -> handleErrorJustOnce(throwable, errorMessage)
+        );
+    }
+
+    // CONSTRUCTORS
+
+    public CompletableFutureErrorHandler(CustomizedLogger debugLogger) {
+        this.debugLogger = debugLogger;
+
+        this.handledExceptions = Collections.synchronizedSet(new IdentityHashMap<Throwable, Object>().keySet());
+    }
+
+    // PRIVATE
+
+    private final Set<Throwable> handledExceptions;
+    private final CustomizedLogger debugLogger;
+
+    private void handleErrorJustOnce(Throwable error, String errorMessage) {
+        if (handledExceptions.add(error))
+            debugLogger.logMessage(errorMessage);
+    }
+
+}
